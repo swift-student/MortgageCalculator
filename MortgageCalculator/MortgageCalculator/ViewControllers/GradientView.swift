9956269cda55252a398fb7bcf877b6777f233de5
Shawn Gee <shawngee@gmail.com>
2020-03-03 17:49:21 -0500

    Added visual details to timeline view

---
+++ MortgageCalculator/MortgageCalculator/ViewControllers/GradientView.swift
@@ -0,0 +1,60 @@
+//
+//  GradientView.swift
+//  MortgageCalculator
+//
+//  Created by Shawn Gee on 3/3/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var secondColor: UIColor = UIColor(white: 1.0, alpha: 0) {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var isHorizontal: Bool = true {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor, secondColor, firstColor].map { $0.cgColor }
+        
+        if isHorizontal {
+            layer.startPoint = CGPoint(x: 0, y: 0)
+            layer.endPoint = CGPoint(x: 1, y: 0)
+        } else {
+           layer.startPoint = CGPoint(x: 0, y: 0)
+            layer.endPoint = CGPoint(x: 0, y: 1)
+        }
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
