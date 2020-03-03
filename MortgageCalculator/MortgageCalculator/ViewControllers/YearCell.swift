6868c508e92a7eb0740966621a2b487f4388db4a
Shawn Gee <shawngee@gmail.com>
2020-03-03 16:52:43 -0500

    Added timeline

---
+++ MortgageCalculator/MortgageCalculator/ViewControllers/YearCell.swift
@@ -0,0 +1,13 @@
+//
+//  YearCell.swift
+//  MortgageCalculator
+//
+//  Created by Shawn Gee on 3/3/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import UIKit
+
+class YearCell: UICollectionViewCell {
+    @IBOutlet weak var yearLabel: UILabel!
+}
