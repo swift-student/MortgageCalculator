024425a32ea336a9ba9df89ab39d459088400641
Shawn Gee <shawngee@gmail.com>
2020-03-04 09:33:42 -0500

    Refactored timeline to child view controller

---
+++ MortgageCalculator/MortgageCalculator/Graphs/BarGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  BarGraphView.swift
+//  MortgageCalculator
+//
+//  Created by Shawn Gee on 3/4/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+import SwiftUI
+
+struct BarGraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BarGraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarGraphView()
+    }
+}
